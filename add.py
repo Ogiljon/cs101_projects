3f1a7a0d1c2576afc7c315875c9983545850854a
O'g'iljon Otajanova <ogiljonotajonova28@gmail.com>
2025-10-02 15:34:37 +0500

    Add intro.py, a simple program

---
+++ add.py
@@ -0,0 +1,3 @@
+first_number = input("Enter the first number: ")
+second_number = input ("Enter the second number: ")
+print("The sum of your numbers is: " , int(first_number) + int(second_number))
---
+++ intro.py
@@ -0,0 +1,3 @@
+print ("Hello, my name is O'g'iljon Otajanova")
+print ("My major is Artificial Intelligtence")
+print ("My favourite hobby is reading books")
